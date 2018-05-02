75fcd8addfa74b7fd03771949bfa66c2401ceb2f
carlos anaya <cfarlos@gmail.com>
2018-05-01 20:21:32 -0600

    formulario login

--- Principal/Principal/frmLogin.cs
+++ Principal/Principal/frmLogin.cs
@@ -15,14 +15,47 @@ namespace Principal
         public frmLogin()
         {
             InitializeComponent();
+            pbImagen.Image = Image.FromFile("login.png");
+            pbUsuario.Image = Image.FromFile("usuario.png");
+            pbContraseña.Image = Image.FromFile("candado.png");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (Data.Login(textBox1.Text, textBox2.Text))
+            if (Data.Login(txtLusuario.Text, txtLcontraseña.Text))
             {
                 this.Hide();
                 new Principal().Show();
-    }        }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLusuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLcontraseña_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLusuario_Validated(object sender, EventArgs e)
+        {
+        }
     }
 }
